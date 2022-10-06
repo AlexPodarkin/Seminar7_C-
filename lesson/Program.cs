1091d01b0ecc099a62830d9c04607b7730ac138d
Aleksandr <alexchembarov@gmail.com>
2022-10-06 20:49:28 +0400

    lesson

--- lesson/Program.cs
+++ lesson/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+/* Console.Write("Введите размер массива для разворота:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array, 0, 99);
+PrintArray(array);
+ReversMassive(array);
+PrintArray(array);
+
+void FillArray(int[] value, int min = 0, int max = 10)
+{
+    for (int i = 0; i < value.Length; i++)
+    {
+        value[i] = new Random().Next(min, max + 1);
+    }
+}
+
+void PrintArray(int[] value)
+{
+    Console.Write("[");
+    for (int i = 0; i < value.Length; i++)
+    {
+        Console.Write(" " + value[i] + " ");
+    }
+    Console.WriteLine("]");
+}
+
+void ReversMassive(int[] value) */
+{
+    int size = value.Length - 1;
+    for (int i = 0; i < value.Length / 2; i++)
+    {
+        (value[i], value[size - i]) = (value[size - i], value[i]);
+    }
+}
