f0c5e1709e4a9c230d9ff5ddc223fd369cc9bf46
Aleksandr <alexchembarov@gmail.com>
2022-10-07 18:53:00 +0400

    HomeWork

---
+++ HomeWork/Program.cs
@@ -0,0 +1,63 @@
+﻿
+Console.Write("Введите количество строк двумерного массива: ");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов двумерного массива: ");
+int columnCount = int.Parse(Console.ReadLine());
+
+double[,] array = FillArray(rowCount, columnCount);
+PrintArray(array);
+
+double[,] FillArray(int rows, int columns)
+{
+    double[,] filledArray = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = new Random().NextDouble() * 100;
+            filledArray[i, j] = Math.Round(filledArray[i, j], 2);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(double[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write($"\t{inputArray[i, j]}");
+        }
+        Console.Write(" ]");
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите номер строки массива элемента для вывода: ");
+int rowEl = int.Parse(Console.ReadLine());
+
+Console.Write("Введите номер столбца массива элемента для вывода: ");
+int columnEl = int.Parse(Console.ReadLine());
+Console.WriteLine($"Значение по запросу индекса = {array[rowEl, columnEl]}");
+
+double[,] AverageArray(double[,] arrayEd)
+{
+    double sumColum = 0;
+    double average = 0;
+
+    for (int j = 0; j < arrayEd.GetLength(1); j++)
+    {
+        for (int i = 0; i < arrayEd.GetLength(0); i++)
+        {
+            sumColum += arrayEd[i, j];
+        }
+        average = Math.Round(sumColum / arrayEd.GetLength(0), 2);
+        Console.WriteLine($" Среднее арифметическое элементов в {j + 1} столбце = {average}");
+        sumColum = 0;
+    }
+    return arrayEd;
+}
+AverageArray(array);
--- lesson/Program.cs
+++ lesson/Program.cs
@@ -1,37 +1,76 @@
-﻿Console.Clear();
+﻿
+Console.Clear();
 
-/* Console.Write("Введите размер массива для разворота:");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[size];
-FillArray(array, 0, 99);
+Console.Write("Введите количество строк двумерного массива: ");
+int rowCount = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов двумерного массива: ");
+int columnCount = int.Parse(Console.ReadLine());
+
+int[,] array = FillArray(rowCount, columnCount, 1, 9);
 PrintArray(array);
-ReversMassive(array);
-PrintArray(array); */
 
-void FillArray(int[] value, int min = 0, int max = 10)
+int[,] FillArray(int rows, int columns, int min, int max)
 {
-    for (int i = 0; i < value.Length; i++)
+    int[,] filledArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
     {
-        value[i] = new Random().Next(min, max + 1);
+        for (int j = 0; j < columns; j++)
+        {
+            filledArray[i, j] = i + j;
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(" " + inputArray[i, j]);
+        }
+        Console.WriteLine();
     }
 }
 
-void PrintArray(int[] value)
+FillArra(array);
+int[,] FillArra(int[,] filladArra)
 {
-    Console.Write("[");
-    for (int i = 0; i < value.Length; i++)
-    {
-        Console.Write(" " + value[i] + " ");
-    }
-    Console.WriteLine("]");
-}
 
-void ReversMassive(int[] value)
-{
-    int size = value.Length - 1;
-    for (int i = 0; i < value.Length / 2; i++)
+    for (int i = 0; i < filladArra.GetLength(0); i++)
     {
-        (value[i], value[size - i]) = (value[size - i], value[i]);
+        for (int j = 0; j < filladArra.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                filladArra[i, j] = filladArra[i, j] * filladArra[i, j];
+            }
+        }
     }
+    return filladArra;
 }
- 
+Console.WriteLine();
+PrintArray(array);
+System.Console.WriteLine();
+
+int[,] SumDia(int[,] filladArra)
+{
+    int summ = 0;
+
+    for (int i = 0; i < filladArra.GetLength(0); i++)
+    {
+        for (int j = 0; j < filladArra.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                summ += filladArra[i, j];
+            }
+        }
+    }
+    System.Console.WriteLine(summ);
+    return filladArra;
+}
+SumDia(array);
